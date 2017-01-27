e210795cc1a2cda1e338821ca28b2a2d0704d408
rghossi <rafael.ghossi@gmail.com>
2017-01-27 00:13:36 -0200

    configuring webpack

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import Layout from './components/Layout';
 import IndexPage from './components/IndexPage';
-import ContestPage from './components/ContestPage';
+// import ContestPage from './components/ContestPage';
 import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
   <Route path="/" component={Layout}>
     <IndexRoute component={IndexPage}/>
-    // <Route path="contest/:id" component={ContestPage}/>
+    <Route path="contest/:id" component={ContestPage}/>
     <Route path="*" component={NotFoundPage}/>
   </Route>
 );
