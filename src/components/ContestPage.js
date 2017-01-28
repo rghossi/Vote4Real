3fddaeca732fcef687c93325c7fc30ac7a16e89e
rghossi <rafael.ghossi@gmail.com>
2017-01-27 23:00:12 -0200

    added contest page component

---
+++ src/components/ContestPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Grid, Row, Col, Clearfix } from 'react-bootstrap';
+import contests from '../data/contests';
+import VotesGraph from './ContestPreview';
+
+export default class ContestPage extends React.Component {
+  render() {
+    const id = this.props.params.id;
+    const contest = contests.filter((contest) => contest.id === id)[0];
+    if (!contest) {
+      return <NotFoundPage/>;
+    }
+    return (
+      <Grid>
+        <Row className="show-grid">
+          <Col md={6} mdPull={6}>
+            <h2>{contest.name}</h2>
+          </Col>
+          <Clearfix visibleSmBlock></Clearfix>
+          <Col md={6} mdPush={6}>
+            <VotesGraph contest={contest} />
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
