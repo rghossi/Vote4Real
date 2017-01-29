06266adc69d79f25dc1a35d8847a617ddeaa095b
rghossi <rafael.ghossi@gmail.com>
2017-01-29 13:45:34 -0200

    added fixed footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,22 +5,27 @@ import { Grid, PageHeader, Row } from 'react-bootstrap';
 export default class Layout extends React.Component {
   render() {
     return (
-      <Grid>
-        <header>
-          <Link to="/">
-          <PageHeader>
-            <h1>Vote4Real</h1>
-          </PageHeader>
-            
-          </Link>
-        </header>
-        <Row className="show-grid">{this.props.children}</Row>
-        <footer>
-          <p>
-            Demo SPA to showcase <strong>React</strong>, <strong>Node</strong>, <strong>Express</strong> and <strong>MongoDB</strong>.
-          </p>
+      <div>
+        <Grid>
+          <header>
+            <Link to="/">
+            <PageHeader>
+              <h1>Vote4Real</h1>
+            </PageHeader>
+              
+            </Link>
+          </header>
+          <Row className="show-grid">{this.props.children}</Row>
+
+        </Grid>
+        <footer className="footer">
+          <Grid>
+            <p className="text-muted">
+              Demo SPA to showcase <strong>React</strong>, <strong>Node</strong>, <strong>Express</strong> and <strong>MongoDB</strong>.
+            </p>
+          </Grid>
         </footer>
-      </Grid>
+      </div>
     );
   }
 }
