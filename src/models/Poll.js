1708245fab3232ad69237a52bb732624527bc4a1
rghossi <rafael.ghossi@gmail.com>
2017-01-30 19:38:53 -0200

    added poll model

---
+++ src/models/Poll.js
@@ -0,0 +1,28 @@
+import Mongoose from 'mongoose';
+
+const Schema = Mongoose.Schema;
+
+const PollSchema = new Schema({
+	title: { type: String, trim: true },
+	options: [{
+		id: Number,
+		desc: { type: String, trim: true },
+		count: { type: Number, default: 0 },
+		users: [{ type: Schema.ObjectId, ref: 'User' }]
+	}],
+	totalVotes: { type: Number, default: 0 },
+	createdAt: { type: Date, default: Date.now }
+});
+
+PollSchema.methods = {
+	saveVote: function(optionId, userId){
+		const index = this.options.map(option => option.id).indexOf(optionId);
+		if (index) {
+			this.options[index].count++;
+			this.options[index].users.push(userId);
+			return this.save();
+		} else {
+			throw new Error('Option not found!');
+		}
+	}
+}
